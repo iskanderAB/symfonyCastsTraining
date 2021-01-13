3de892566f43c8fdbbfa1ffd72bb31256a5d9546
iskanderAB <iskanderabasi@gmail.com>
2021-01-13 22:57:55 +0100

    first_commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+
+    /**
+     *@Route("/login",name="app_login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        return $this->json([
+            'user' => $this->getUser() ? $this->getUser()->getId():null
+        ]);
+    }
+}
